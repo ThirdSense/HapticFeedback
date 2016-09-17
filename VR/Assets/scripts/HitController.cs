04b9f1ce0b2889711f6829eac592d0a1b86cd632
Nico <5westbury5@gmail.com>
2016-09-17 08:05:57 -0400

    commited all missing files

---
+++ VR/Assets/scripts/HitController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class HitController : MonoBehaviour {
+
+    public Transform projectile;
+
+    private Vector3 spawn_pos;
+
+    void Start()
+    {
+        SpawnProjectile();
+    }
+
+    void OntriggerEnter(Collision other)
+    {
+        Debug.Log("Hit");
+        Destroy(other.gameObject);
+        SpawnProjectile();            
+    }
+
+    void SpawnProjectile()
+    {
+        Random.InitState((int)System.DateTime.Now.Ticks);
+
+        float x = Random.Range(-20f, 20f);
+        float y = Random.Range(3f, 10f);
+        float z = Random.Range(3f, 30f);
+        spawn_pos = new Vector3(x, y, z);
+
+        Object.Instantiate(projectile, spawn_pos, Quaternion.identity);
+    }
+}
+
---
+++ VR/Assets/scripts/projectileControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileControl : MonoBehaviour {
+
+    public float speed;
+
+    private Camera player;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = Camera.main;
+
+        Vector3 player_pos = player.transform.position;
+
+        Vector3 direction = (player_pos - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
+
+    void OntriggerEnter(Collision col)
+    {
+        Debug.Log("test");
+    }
+}
