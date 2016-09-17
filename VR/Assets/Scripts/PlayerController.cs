845b2c38bf87cc0cf4488fc687f0c3f1a6fe1263
jhryniw <jhryniw@gmail.com>
2016-09-17 08:09:31 -0400

    Merged changes

---
+++ VR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject head;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = head.transform.position - transform.position;
+    }
+
+	void Update()
+    {
+        transform.position = head.transform.position + offset;
+    }
+}
